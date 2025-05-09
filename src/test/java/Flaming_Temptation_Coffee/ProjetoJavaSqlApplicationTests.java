19dac102d438ada6a084492989cad79b3a8bdfb1
Stephany Paz <stephanypazb@gmail.com>
2025-05-09 02:20:37 -0300

    Commit inicial

---
+++ src/test/java/Flaming_Temptation_Coffee/ProjetoJavaSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package Flaming_Temptation_Coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
